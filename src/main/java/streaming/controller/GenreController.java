6515691ef46403ccf83e78076169227848b2f96c
tmpUsername <admin@fddf-PC>
2016-10-10 10:27:17 +0200

    GenreController.java: ajout de l'annotation string pour lister

--- src/main/java/streaming/controller/GenreController.java
+++ src/main/java/streaming/controller/GenreController.java
@@ -6,6 +6,8 @@
 package streaming.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class GenreController {
     
+    @RequestMapping(value = "/lister_genres", method = RequestMethod.GET)
     public String lister(){
         return "genre_lister.jsp";
     }
